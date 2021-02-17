8e7cbdc218f4b61184933427714222520a866868
David Fekke <david@fekke.com>
2021-02-17 12:05:01 -0500

    Adding changes to the twitter scripts

--- twitter.js
+++ twitter.js
@@ -1,43 +1,16 @@
-let timerId;
-let lastUpdated;
-let currentTweet;
-let lastTweet;
 
 import twitter from 'twitter';
 
-var client = new twitter({
+const client = new twitter({
   consumer_key: process.env.twitter_ck,
   consumer_secret: process.env.twitter_cs,
   access_token_key: process.env.twitter_atk,
   access_token_secret: process.env.twitter_ats,
 });
 
-/**
- * Stream statuses filtered by keyword
- * number of tweets per second depends on topic popularity
- **/
-// client.stream('statuses/filter', {track: '$GME'},  function(stream){
-//     stream.on('data', function(tweet) {
-//         console.log(tweet.text);
-//     });
-
-//     stream.on('error', function(error) {
-//         console.log(error);
-//     });
-// });
-
-// client.stream('statuses/filter', {track: '$AAPL'},  function(stream){
-//     stream.on('data', function(tweet) {
-//         console.log(tweet.text);
-//     });
-  
-//     stream.on('error', function(error) {
-//         console.log(error);
-//     });
-// });
-
 client.stream('statuses/filter', {track: '$PLTR'},  function(stream){
     stream.on('data', function(tweet) {
+        console.log(tweet);
         console.log(tweet.text);
     });
 
