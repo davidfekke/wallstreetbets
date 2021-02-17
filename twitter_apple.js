51ccf15516a1a214e0c606c51efc4c358895e3e3
David Fekke <david@fekke.com>
2021-02-17 11:59:49 -0500

    Adding twitter bots

---
+++ twitter_apple.js
@@ -0,0 +1,47 @@
+let timerId;
+let lastUpdated;
+let currentTweet;
+let lastTweet;
+
+import twitter from 'twitter';
+
+var client = new twitter({
+  consumer_key: process.env.twitter_ck,
+  consumer_secret: process.env.twitter_cs,
+  access_token_key: process.env.twitter_atk,
+  access_token_secret: process.env.twitter_ats,
+});
+
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+// client.stream('statuses/filter', {track: '$GME'},  function(stream){
+//     stream.on('data', function(tweet) {
+//         console.log(tweet.text);
+//     });
+
+//     stream.on('error', function(error) {
+//         console.log(error);
+//     });
+// });
+
+// client.stream('statuses/filter', {track: '$AAPL'},  function(stream){
+//     stream.on('data', function(tweet) {
+//         console.log(tweet.text);
+//     });
+  
+//     stream.on('error', function(error) {
+//         console.log(error);
+//     });
+// });
+
+client.stream('statuses/filter', {track: '$AAPL'},  function(stream){
+    stream.on('data', function(tweet) {
+        console.log(tweet.text);
+    });
+
+    stream.on('error', function(error) {
+        console.log(error);
+    });
+});
